b389b0ca7ff3c47f73a92e70ab428dfbd7c13eda
Noopur <noopur@Roambees-MacBook-Pro.local>
2018-01-18 16:42:20 +0530

    primeNG with server side loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { SliderModule } from 'primeng/slider';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SliderModule,
+    DropdownModule,
+    MultiSelectModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
